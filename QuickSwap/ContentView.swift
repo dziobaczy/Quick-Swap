a4250cb4ad2ac59c54204b8effbafcd28c02c8d7
OfftheCode <offthecode@gmail.com>
2020-07-09 03:31:07 +0200

    Initial Commit

---
+++ QuickSwap/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  QuickSwap
+//
+//  Created by Piotr Szadkowski on 09/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuickSwap/QuickSwapApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickSwapApp.swift
+//  QuickSwap
+//
+//  Created by Piotr Szadkowski on 09/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct QuickSwapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
