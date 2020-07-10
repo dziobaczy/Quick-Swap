00379e6b3f021024e19ef87373f613eb27f770de
OfftheCode <offthecode@gmail.com>
2020-07-10 15:27:58 +0200

    Add CurrencyListView

---
+++ QuickSwap/Currency/CurrencyListView.swift
@@ -0,0 +1,30 @@
+//
+//  CurrencyListView.swift
+//  QuickSwap
+//
+//  Created by Piotr Szadkowski on 10/07/2020.
+//
+
+import SwiftUI
+
+struct CurrencyListView: View {
+    var body: some View {
+        NavigationView {
+            List {
+                CurrencySwapView()
+                    .padding(.bottom, 35)
+                    .padding(.top, 15)
+                ForEach(0..<10) { i in
+                    CurrencyRowView()
+                }
+                .navigationBarTitle("Currencies")
+            }
+        }
+    }
+}
+
+struct CurrencyListView_Previews: PreviewProvider {
+    static var previews: some View {
+        CurrencyListView()
+    }
+}
